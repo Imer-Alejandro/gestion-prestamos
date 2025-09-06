734f76c4231ab3640500be3b31f816021cec9cb7
imer alejandro <consymer1@gmail.com>
2025-09-06 18:31:07 +0200

    implementacion de la configuracion del firebase

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import "../global.css";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center ">
       <Text className="text-xl font-bold text-blue-500">
         vamos hacernos rico con esta app!
       </Text>
